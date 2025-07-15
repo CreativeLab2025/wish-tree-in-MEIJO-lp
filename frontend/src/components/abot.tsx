10b623512c94aa046227fdf19196129da29e42c4
aya <116853251+yamamoto-ayano@users.noreply.github.com>
2025-07-16 00:14:13 +0900

    aboutいい感じにした

--- frontend/src/components/abot.tsx
+++ frontend/src/components/abot.tsx
@@ -3,16 +3,16 @@ import Link from 'next/link';
 
 export default function Abot() {
   return (
-    <section className="max-w-3xl ml-auto h-screen flex flex-col justify-center py-24 px-10 md:py-32 md:px-20 bg-orange-400 rounded-3xl shadow-2xl text-left">
-      <h2 className="text-4xl md:text-6xl font-extrabold mb-6 text-white drop-shadow">about product</h2>
-      <h3 className="text-2xl md:text-4xl font-bold mb-10 text-white drop-shadow">Wish tree in MEIJOへようこそ</h3>
-      <p className="mb-6 text-xl md:text-2xl text-white leading-relaxed tracking-wide">
+    <section className="z-[100] max-w-3xl h-200px ml-auto flex flex-col justify-center py-80 my-32 px-10 md:py-80 md:px-20 bg-[#F7F1E8] rounded-3xl shadow-2xl text-left">
+      <h2 className="text-4xl md:text-6xl font-extrabold mb-6 text-black drop-shadow"style={{ lineHeight: 10.0 }}>about product</h2>
+      <h3 className="text-2xl md:text-4xl font-bold mb-10 text-black drop-shadow"style={{ lineHeight: 2.5 }}>Wish tree in MEIJOへようこそ</h3>
+      <p className="mb-12 text-xl md:text-2xl text-black tracking-wide" style={{ lineHeight: 2.5 }}>
         名城大学100周年を記念して作成されたこのプロダクト。
       </p>
-      <p className="mb-6 text-xl md:text-2xl text-white leading-relaxed tracking-wide">
+      <p className="mb-12 text-xl md:text-2xl text-black tracking-wide" style={{ lineHeight: 2.5 }}>
         この木にお願い事を書いた実を投げ込むと、そのお願いが叶うというものです。
       </p>
-      <p className="mb-12 text-xl md:text-2xl text-white leading-relaxed tracking-wide">
+      <p className="mb-12 text-xl md:text-2xl text-back tracking-wide" style={{ lineHeight: 2.5 }}>
         皆さんこの木にお願い事を投げ込んでみませんか？
       </p>
       <Link href="#ar">
