e475ada13f9e7de222562abf7fe22f1e44f1a65b
ree-jp <mail@ree-jp.net>
2025-07-20 00:12:29 +0900

    fix: cssを上手く読み込んでくれないのを修正

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: isProd ? '/wish-tree-in-MEIJO-lp' : '',
+  assetPrefix: isProd ? '/wish-tree-in-MEIJO-lp/' : '',
   images: {
     unoptimized: true
   }
